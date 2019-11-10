0b8ba02f2e0b29cee92f82b1ef30d2673dca15f9
Alexandru Copot <alex.mihai.c@gmail.com>
2019-11-10 12:24:54 +0100

    Apply cargo fmt
    Signed-off-by: Alexandru Copot <alex.mihai.c@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 
 extern crate panic_halt;
 
-use riscv_rt::entry;
 use k210_hal::prelude::*;
-use k210_hal::Peripherals;
 use k210_hal::stdout::Stdout;
+use k210_hal::Peripherals;
+use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
@@ -16,7 +16,9 @@ fn main() -> ! {
     let p = Peripherals::take().unwrap();
 
     // Configure UART
-    let serial = p.UARTHS.configure((p.pins.pin5, p.pins.pin4), 115_200.bps(), &clocks);
+    let serial = p
+        .UARTHS
+        .configure((p.pins.pin5, p.pins.pin4), 115_200.bps(), &clocks);
     let (mut tx, _) = serial.split();
 
     let mut stdout = Stdout(&mut tx);
